20ddb795360384b2e74462648cbc858b3d551870
lisamaison <lisamaison@hotmail.com>
2017-03-21 05:27:07 -0400

    heroku3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   root 'students'
+   root :to => 'login#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
