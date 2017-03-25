906a4f9414a5a62d70b39add038fe3f0ab94150c
lisamaison <lisamaison@hotmail.com>
2017-03-25 05:00:36 -0400

    VILLE

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :villes
-
   resources :students
-   root :to => 'students#index'
+   root :to => 'VILLE#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
