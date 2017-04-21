6192b422b7040cc13ca95580e3ad0c81c63bce5c
lisamaison <lisamaison@hotmail.com>
2017-04-21 11:38:03 -0400

    tuto2

--- applicationlisa/src/app/mock-heroes.ts
+++ applicationlisa/src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
     import { Hero } from './hero';
     export const HEROES: Hero[] = [
-      {id: 11, name: 'Mr. Nice'},
-      {id: 12, name: 'Narco'},
-      {id: 13, name: 'Bombasto'},
-      {id: 14, name: 'Celeritas'},
-      {id: 15, name: 'Magneta'},
+      {id: 11, name: 'Jonny'},
+      {id: 12, name: 'Domi'},
+      {id: 13, name: 'Remy'},
+      {id: 14, name: 'Thierry'},
+      {id: 15, name: 'Emily'},
       {id: 16, name: 'RubberMan'},
       {id: 17, name: 'Dynama'},
       {id: 18, name: 'Dr IQ'},
       {id: 19, name: 'Magma'},
       {id: 20, name: 'Tornado'}
-    ];
+];
